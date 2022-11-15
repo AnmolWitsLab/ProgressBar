4c5e24c61603579e6cca3db0746aa329fc40f0a7
Anmol <anmol.singh@thewitslab.com>
2022-11-15 19:10:47 +0530

    createprogressbarui

--- App.js
+++ App.js
@@ -1,53 +1,96 @@
-import React from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  Dimensions,
-  TouchableHighlight,
-} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
 
 const App = () => {
+  const [progress, setProgress] = useState(20);
+
   return (
     <View style={styles.container}>
-      <TouchableHighlight style={styles.circle}>
-        <Text style={styles.text}>Circle</Text>
-      </TouchableHighlight>
-      {/* <View style={styles.example}>
-        <Text>Horizontal Progress Indicator</Text>
+      <Text style={styles.label}>Progress {progress}%</Text>
+
+      <View style={styles.progressBG}>
+        <View
+          style={[
+            styles.progress,
+            {
+              width: `${progress}%`,
+            },
+          ]}
+        />
       </View>
-      <View style={styles.example}>
-        <Text>Colored Progress Indicator</Text>
+
+      <View style={styles.btnBox}>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => {
+            if (progress > 20) {
+              setProgress(progress - 20);
+            }
+          }}>
+          <Text style={styles.btnText}>Previous</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => {
+            if (progress < 100) {
+              setProgress(progress + 20);
+            }
+          }}
+          style={styles.btn}>
+          <Text style={styles.btnText}>Next</Text>
+        </TouchableOpacity>
       </View>
-      <View style={styles.example}>
-        <Text>Fixed Progress Value</Text>
-      </View> */}
     </View>
   );
 };
 
+export default App;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-  },
-  circle: {
-    borderRadius: Math.round(
-      Dimensions.get('window').width + Dimensions.get('window').height,
-    ),
-    width: Dimensions.get('window').width * 0.5,
-    height: Dimensions.get('window').width * 0.5,
-    borderWidth: 2,
-    backgroundColor: 'yellow',
     justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: '#E0EAE9',
   },
 
-  text: {
-    fontSize: 20,
-    color: 'black',
+  progressBG: {
+    width: '20%',
+    height: '10%',
+    backgroundColor: '#C4CDD5',
+    marginHorizontal: 25,
+    borderRadius: 50,
+  },
+
+  progress: {
+    width: '20%',
+    height: '100%',
+    backgroundColor: '#00AB55',
+    borderRadius: 50,
+  },
+
+  label: {
+    fontSize: 15,
+    fontWeight: '500',
+    color: '#005249',
+    marginBottom: 20,
+  },
+
+  btn: {
+    paddingVertical: 8,
+    paddingHorizontal: 15,
+    backgroundColor: '#919EAB',
+    borderRadius: 6,
+    marginHorizontal: 10,
+    marginTop: 40,
+  },
+
+  btnText: {
+    fontWeight: '500',
+    color: '#fff',
+  },
+
+  btnBox: {
+    flexDirection: 'row',
   },
 });
-
-export default App;
---
+++ data.js
@@ -0,0 +1,36 @@
+export default [
+  {
+    id: 1,
+    name: 'AADHAR CARD',
+    description: 'GOVERNMENT OF INDIA',
+    color: 'red',
+  },
+  {
+    id: 2,
+    name: 'PAN CARD',
+    description: 'GOVERNMENT OF INDIA',
+    color: 'green',
+  },
+  {
+    id: 3,
+    name: 'UAF REGISTRATION VC',
+    description: 'GOVERNMENT OF INDIA',
+    color: 'grey',
+  },
+  {
+    id: 4,
+    name: 'CONSENT',
+    description: 'GOVERNMENT OF INDIA',
+    color: 'yellow',
+  },
+  {
+    id: 5,
+    name: 'PHONE NUMBER VERIFICATION',
+    description: 'GOVERNMENT OF INDIA',
+    color: 'green',
+    phone: 'Phone Number:',
+    number: '+918219066845',
+    verify: 'Verify Status:',
+    verified: 'Verified',
+  },
+];
